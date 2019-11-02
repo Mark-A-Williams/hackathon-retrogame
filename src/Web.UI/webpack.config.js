779a3be495e1c06025963bd6e6db9a19e7a0b54b
Joshua Yates <josht.yates@btinternet.com>
2019-11-02 10:24:45 +0000

    Add initial frontend files

---
+++ src/Web.UI/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: "./index.ts",
+    output: {
+        path: "../Web/wwwroot",
+        filename: "[name].[chunkhash].js",
+        publicPath: "/"
+    },
+    resolve: {
+        extensions: [".js", ".ts"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(["../Web/wwwroot/*"]),
+        new HtmlWebpackPlugin({
+            template: "./index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "styles/[name].[chunkhash].css"
+        })
+    ]
+};
