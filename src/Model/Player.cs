8dd3fed19b6ed0da7dffbded14fcf7cfff44fb65
Mark Williams <mark.williams@enable.com>
2019-11-02 12:17:48 +0000

    maths

--- src/Model/Player.cs
+++ src/Model/Player.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Model
 {
@@ -6,7 +7,7 @@ namespace Model
     {
         public int Index { get; }
         public string Color { get; }
-        public float Position { get; }
+        public float Position { get; } // position of centre of paddle from 0 to 1
 
         public Player(int index, string color, float position)
         {
@@ -15,14 +16,32 @@ namespace Model
             Position = position;
         }
 
-        public Vector GetCentreCoordsFromPlayerIndex(int index, int numPlayers)
+        public Vector GetCentreCoordsFromPlayerIndex(int index)
         {
+            // index: 0 to N-1 (N players)
+            var numPlayers = 8; // will get this from gamestate, could be any int!
             var theta_n = 2*Math.PI*index/numPlayers;
             var phi = Math.Cos(Math.PI/numPlayers);
             var xPos = phi*Math.Cos(theta_n);
             var yPos = phi*Math.Sin(theta_n); 
-            var centreCoord = new Vector(xPos, yPos);
-            return centreCoord;
+            var centreCoords = new Vector(xPos, yPos);
+            return centreCoords;
+        }
+
+        public List<Vector> GetEndCoordsFromPlayerPosition(double playerPosition)
+        {
+            // this method might live somewhere else eventually hence some slightly redundant code
+            
+            // playerposition: 0+paddleLength/2 to 1-paddleLength/2
+
+            var numPlayers = 8; // will get this from gamestate, could be any int!
+            var paddleLength = 0.3; // will DEFINITELY get from gamestate, but probs about this
+            var centreCoords = GetCentreCoordsFromPlayerIndex(Index);            
+
+            var end_1 = new Vector(0,0);
+            var end_2 = new Vector(0,0);
+            var ends = new List<Vector>{end_1, end_2};
+            return ends;
         }
     }
 }
