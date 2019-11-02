0cfae3cbe48578834c5e2ba432cc99d3b5ca1d89
Mark Williams <mark.williams@enable.com>
2019-11-02 13:06:32 +0000

    maths and restructuring

--- src/Model/Player.cs
+++ src/Model/Player.cs
@@ -8,40 +8,17 @@ namespace Model
         public int Index { get; }
         public string Color { get; }
         public float Position { get; } // position of centre of paddle from 0 to 1
+        public PlayerArea PlayerArea { get; set; }
+        public List<Vector> PaddleEndCoords {get; set; }
 
         public Player(int index, string color, float position)
         {
             Index = index;
             Color = color;
             Position = position;
+            PlayerArea = new PlayerArea(index);
         }
 
-        public Vector GetCentreCoordsFromPlayerIndex(int index)
-        {
-            // index: 0 to N-1 (N players)
-            var numPlayers = 8; // will get this from gamestate, could be any int!
-            var theta_n = 2*Math.PI*index/numPlayers;
-            var phi = Math.Cos(Math.PI/numPlayers);
-            var xPos = phi*Math.Cos(theta_n);
-            var yPos = phi*Math.Sin(theta_n); 
-            var centreCoords = new Vector(xPos, yPos);
-            return centreCoords;
-        }
-
-        public List<Vector> GetEndCoordsFromPlayerPosition(double playerPosition)
-        {
-            // this method might live somewhere else eventually hence some slightly redundant code
-            
-            // playerposition: 0+paddleLength/2 to 1-paddleLength/2
-
-            var numPlayers = 8; // will get this from gamestate, could be any int!
-            var paddleLength = 0.3; // will DEFINITELY get from gamestate, but probs about this
-            var centreCoords = GetCentreCoordsFromPlayerIndex(Index);            
-
-            var end_1 = new Vector(0,0);
-            var end_2 = new Vector(0,0);
-            var ends = new List<Vector>{end_1, end_2};
-            return ends;
-        }
+        
     }
 }
