59b6020ec95c9cb6a138c8c88de94ac78076fb06
Matthew Sharpe <mds@gn.apc.org>
2019-11-02 10:53:47 +0000

    game engine

---
+++ src/Model/GameState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Model
+{
+    public class GameState
+    {
+        public GameState(
+            Ball ball,
+            IEnumerable<Player> players
+        )
+        {
+            Ball = ball;
+            Players = ImmutableList.CreateRange(players);
+        }
+
+        public Ball Ball { get; }
+        public ImmutableList<Player> Players { get; }
+    }
+}
