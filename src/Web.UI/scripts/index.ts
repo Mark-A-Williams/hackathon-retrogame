78644f79a3c3b241122c293675724f2856cbcf14
Joshua Yates <josht.yates@btinternet.com>
2019-11-02 10:45:20 +0000

    Setup communication

--- src/Web.UI/scripts/index.ts
+++ src/Web.UI/scripts/index.ts
@@ -1,9 +1,26 @@
-import "../styles/main.css";
+import "./css/main.css";
+import * as signalR from "@aspnet/signalr";
 
 const divMessages: HTMLDivElement = document.querySelector("#divMessages");
 const tbMessage: HTMLInputElement = document.querySelector("#tbMessage");
 const btnSend: HTMLButtonElement = document.querySelector("#btnSend");
 const username = new Date().getTime();
+
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/hub")
+    .build();
+
+connection.on("messageReceived", (username: string, message: string) => {
+    let messageContainer = document.createElement("div");
+
+    messageContainer.innerHTML =
+        `<div class="message-author">${username}</div><div>${message}</div>`;
+
+    divMessages.appendChild(messageContainer);
+    divMessages.scrollTop = divMessages.scrollHeight;
+});
+
+connection.start().catch(err => document.write(err));
 
 tbMessage.addEventListener("keyup", (e: KeyboardEvent) => {
     if (e.keyCode === 13) {
@@ -14,4 +31,6 @@ tbMessage.addEventListener("keyup", (e: KeyboardEvent) => {
 btnSend.addEventListener("click", send);
 
 function send() {
+    connection.send("newMessage", username, tbMessage.value)
+              .then(() => tbMessage.value = "");
 }
