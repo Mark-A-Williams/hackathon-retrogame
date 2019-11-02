0a5822f214a45e0227af2deb10102c96c924c8b4
Alex Vale <alex.vale@enable.com>
2019-11-02 18:33:07 +0000

    Add colours to server side

---
+++ src/Model/Colours.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public static class Colours
+    {
+        public static List<string> _colours = new List<string>()
+        {
+            "#000000", "#FF0000", "#00FF00", "#0000FF", "#800000", "#FFFF00", "#00FFFF", "#F0F0F0", "#101010"
+        };
+
+        public static string GetColour(int index)
+        {
+            if (index < _colours.Count - 1)
+            {
+                return _colours[index];
+            } else
+            {
+                return GetColour(index - _colours.Count - 1);
+            }
+        }
+
+    }
+}
