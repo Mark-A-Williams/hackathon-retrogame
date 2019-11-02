6350cdfecb45df1b3a2dd7730d60537873dca14a
Mark Williams <mark.williams@enable.com>
2019-11-02 10:20:58 +0000

    Add barebones Ball and Player models.

---
+++ src/Model/Player.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Drawing;
+
+namespace Model
+{
+    public class Player
+    {
+        public int playerIndex { get; set; }
+        public Color color { get; set; }
+        public float playerPosition { get; set; }
+    }
+}
